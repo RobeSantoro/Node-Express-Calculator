7085c8ecb5fce4b0b511815f7b4dab262726c796
Robe Santoro z800 <robe.santoro@live.com>
2022-06-02 02:21:48 +0200

    Calculator ver 1.0

--- calculator.js
+++ calculator.js
@@ -3,16 +3,13 @@ const bodyParser = require('body-parser');
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(__dirname));
+app.use(express.static(__dirname + '/public'));
 
-app.get('/', (req, res) => {
-  //console.log(__dirname);
+app.get('/', (req, res) => {  
   res.sendFile(__dirname + '/index.html');
 });
 
 app.post('/', (req, res) => {
-
-  console.log(req.body);
 
   let num1 = Number(req.body.num1);
   let num2 = Number(req.body.num2);
@@ -20,23 +17,89 @@ app.post('/', (req, res) => {
 
   let result = 0;
 
-  if (operator === '+') {
+  switch (operator) {  
+  case '+':
     result = num1 + num2;
-  } else if (operator === '-') {
+    break;
+  case '-':
     result = num1 - num2;
-  } else if (operator === 'x') {
+    break;
+  case 'x':
     result = num1 * num2;
-  } else if (operator === ':') {
+    break;
+  case ':':
     result = num1 / num2;
-  } else if (operator === 'Select an Operator') {
-    res.send(`<h1>Please select an operator</h1>             
-              <a href="/">New Operation</a>`);
+    break;
+  default:
+    result = 'Invalid operation';
   }
 
-  res.send(`<body style="  background-color: #0bff34;">
-              <h1 align="center" style=" color: #ff0be7;">${num1} ${operator} ${num2} = ${result}</h1>            
-              <a href="/">New Operation</a>
-            </body>`);
+  console.log(`${num1} ${operator} ${num2} = ${result}`);
+
+  // Send the result to the result.html with the variable result
+  res.send(`<!DOCTYPE html>
+  <html lang="en">
+  
+    <head>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <link rel="stylesheet" href="style.css">
+      <title>Calculator</title>
+    </head>
+  
+    <body>
+      <div class="container">
+        <div class="panel">
+          <h1>Calculator</h1>
+          <p>Insert Numbers and select an operation</p>
+          <form action="/" method="post" class="form cf">
+            <div class="operation plan cf ">
+              <input
+                maxlength="7"
+                oninput="javascript: if (this.value.length > this.maxLength) this.value = this.value.slice(0, this.maxLength);"
+                type="number"
+                name="num1"
+                placeholder=""
+                autocomplete="off"
+                value="${num1}"
+              >
+  
+              <div class="custom-select">
+                <input class="hidden" id="+" type="radio" name="operator" value="+">
+                <label for="+">+</label>
+                <input class="hidden" id="-" type="radio" name="operator" value="-">
+                <label for="-">-</label>
+                <input class="hidden" id="x" type="radio" name="operator" value="x">
+                <label for="x">x</label>
+                <input class="hidden" id=":" type="radio" name="operator" value=":">
+                <label for=":">:</label>
+              </div>
+  
+              <input
+                maxlength="7"
+                oninput="javascript: if (this.value.length > this.maxLength) this.value = this.value.slice(0, this.maxLength);"
+                type="number"
+                name="num2"
+                placeholder=""
+                autocomplete="on"     
+                value="${num2}"                        
+              >
+  
+            </div>
+            <button type="submit" name="submit">Calculate</button>
+          </form>
+  
+          <p><strong>${num1} ${operator} ${num2} = ${result}<strong></p>
+  
+        </div>
+      </div>
+      <script>
+        document.getElementById('${operator}').checked = true;
+      </script>
+    </body>
+  
+  </html>`);
 });
 
 app.listen(3000, () => {
